e3a102aa527d868ab5153390bff97af2f6bae4f0
tranquoc113 <tranquoc631@gmail.com>
2021-01-09 09:33:49 +0700

    upldate layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorHandlingModule } from './error-handling/error-handling.module';
 import { ConfigModule } from './config/config.module';
+import { ShareMaterialModule } from './sharematerial.module';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     ConfigModule,
     ErrorHandlingModule,
+    ShareMaterialModule
   ]
 })
 export class SharedModule { }
